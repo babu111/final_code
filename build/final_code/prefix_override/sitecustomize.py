3b13d1f39f722282c3e213da8c35027cb9bc0502
babu111 <zc096373@gmail.com>
2025-03-20 11:15:50 -0700

    added different coordinates for robot 07 and 08

--- build/final_code/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/zeyi19@netid.washington.edu/ningbo_ws/src/final_code/install/final_code'
